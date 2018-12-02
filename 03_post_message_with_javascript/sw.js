2e3e42f2941c59e613f3354a7a9123ab2f6e0220
kiyohiro kawamura <kyorohiro@gmail.com>
2018-12-02 23:02:15 +0900

    add post messagee ex for js

---
+++ 03_post_message_with_javascript/sw.js
@@ -0,0 +1,43 @@
+var cacheName = 'hello_newResponse-v0.0.1';
+
+self.addEventListener('install', function(event) {
+  console.log('# install (1)');
+  const installFunc = function (cache) {
+  	console.log('# install (2)');
+  	return cache.addAll(["/","/index.html"]);
+  };
+  event.waitUntil(caches.open(cacheName).then(installFunc));
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log("# fetch : " + event.request.url);
+	const responceFunc = function(response) {
+		if (response) {
+      return response;
+    }
+    return fetch(event.request);
+	};
+  //https://developer.mozilla.org/en/docs/Web/API/FetchEvent/request
+  //https://developer.mozilla.org/en/docs/Web/API/FetchEvent/respondWith
+  //https://developer.mozilla.org/en/docs/Web/API/Cache/match
+	event.respondWith(caches.match(event.request).then(responceFunc));
+});
+
+
+self.addEventListener('message', function(e) {
+  console.log("# receive message " + e);
+  //
+  // https://developer.mozilla.org/en/docs/Web/API/ServiceWorkerGlobalScope/clients
+  // https://developer.mozilla.org/en/docs/Web/API/Clients
+  self.clients.matchAll().then(clients =>
+    clients.forEach(client => client.postMessage(e.data))
+  );
+}, false);
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(self.skipWaiting()); // Activate worker immediately
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(self.clients.claim()); // Become available to all pages
+});
