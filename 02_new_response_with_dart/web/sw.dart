bc36425f5d8c5fa14efafca41d442c90765264cb
kiyohiro kawamura <kyorohiro@gmail.com>
2018-12-09 00:27:47 +0900

    mod new response sample

--- 02_new_response_with_dart/web/sw.dart
+++ 02_new_response_with_dart/web/sw.dart
@@ -1,31 +1,23 @@
 import 'package:service_worker/worker.dart' as sw;
 
 void main() {
-  String cacheName = "v0.0.2";
-
-  sw.onInstall.listen((sw.InstallEvent e) async {
-    print("##>> install");
-    Future installTask = new Future(() async {
-      sw.Cache cache = await sw.caches.open(cacheName);
-      cache.addAll([
-        "/","/index.html","main.dart.js"
-      ]);
-    });
-    e.waitUntil(installTask);
+  sw.onInstall.listen((sw.InstallEvent e){
+    print("#>>on install");
+    e.waitUntil(sw.skipWaiting());
   });
+
+//
+//  sw.onActivate.listen((sw.ExtendableEvent e){
+//    print("#>>on activate");
+//    e.waitUntil(sw.clients.claim());
+//  });
+//
 
   sw.onFetch.listen((sw.FetchEvent e) {
     print("##>> fetch");
     Future<sw.Response> responseTask = new Future(() async {
-      String lastPath = e.request.url.replaceFirst(RegExp(r".*\/"), "");
-      sw.Response resonseCache = await sw.caches.match(e.request);
-      if(resonseCache != null){
-        return resonseCache;
-      } else if(lastPath != "") {
-        return new sw.Response.custom("## ${lastPath}",status: 200,statusText: "OK");
-      } else {
-        return sw.fetch(e.request);
-      }
+      String path = e.request.url.replaceFirst(RegExp(r"(http|https)://[^/]*"), "");
+      return new sw.Response.custom("### ${path}",status: 200,statusText: "OK");
     });
     e.respondWith(responseTask);
   });
